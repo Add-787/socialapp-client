8bc9304234d9c17346b6a5f501dd7d68dcb3a435
Add-787 <adarshshetty787@gmail.com>
2020-11-02 01:06:42 +0530

    deploy client

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
+import { Button, Confirm, Icon, Popup } from "semantic-ui-react";
+
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+
+export default function DeleteButton({ postId, commentId, callback }) {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const mutation = commentId ? DELETE_COMMENT : DELETE_POST;
+
+  const [deletePostOrComment] = useMutation(mutation, {
+    update(proxy) {
+      setConfirmOpen(false);
+      if (!commentId) {
+        const data = proxy.readQuery({
+          query: FETCH_POSTS_QUERY,
+        });
+        //data.getPosts = data.getPosts.filter((p) => p.id !== postId);
+        proxy.writeQuery({
+          query: FETCH_POSTS_QUERY,
+          data: { getPosts: data.getPosts.filter((p) => p.id !== postId) },
+        });
+      }
+
+      // TODO remove post from cache
+      if (callback) {
+        callback();
+      }
+    },
+    variables: {
+      postId,
+      commentId,
+    },
+  });
+  return (
+    <div>
+      <Popup
+        content={commentId ? "Delete comment" : "Delete post"}
+        inverted
+        trigger={
+          <Button
+            as="div"
+            color="red"
+            floated="right"
+            onClick={() => setConfirmOpen(true)}
+          >
+            <Icon name="trash" basic style={{ margin: 0 }}></Icon>
+          </Button>
+        }
+      />
+      <Confirm
+        open={confirmOpen}
+        onCancel={() => setConfirmOpen(false)}
+        onConfirm={deletePostOrComment}
+      />
+    </div>
+  );
+}
+
+const DELETE_POST = gql`
+  mutation deletePost($postId: ID!) {
+    deletePost(postId: $postId)
+  }
+`;
+
+const DELETE_COMMENT = gql`
+  mutation deleteComment($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId) {
+      id
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      commentCount
+    }
+  }
+`;
